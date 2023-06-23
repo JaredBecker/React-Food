c1d6c4135ac02f4be321509c61f850db1003b8ee
Jared Becker <jaredbecker46@gmail.com>
2023-06-23 15:38:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Layout/Header";
+
+function App() {
+    return (
+        <>
+            <h2>Let's get started!</h2>
+            <Header />
+        </>
+    );
+}
+
+export default App;
